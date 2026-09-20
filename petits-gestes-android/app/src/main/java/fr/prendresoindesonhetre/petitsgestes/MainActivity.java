ea25637cfd193d532e9ceff02fc3b03d1e443095
prendresoindesonhetre-rgb <prendresoindesonhetre@gmail.com>
2026-09-20 12:15:22 +0200

    Petits gestes: add petits-gestes-android/app/src/main/java/fr/prendresoindesonhetre/petitsgestes/MainActivity.java

---
+++ petits-gestes-android/app/src/main/java/fr/prendresoindesonhetre/petitsgestes/MainActivity.java
@@ -0,0 +1,64 @@
+package fr.prendresoindesonhetre.petitsgestes;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(244,239,249));
+        getWindow().setNavigationBarColor(Color.rgb(251,248,244));
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(244,239,249));
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setMediaPlaybackRequiresUserGesture(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setSupportZoom(false);
+        settings.setTextZoom(100);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String scheme = request.getUrl().getScheme();
+                return scheme != null && (scheme.equals("http") || scheme.equals("https"));
+            }
+        });
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+}
